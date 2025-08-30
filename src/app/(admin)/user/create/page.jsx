24ddfc6bc7ec48b2dbd85c06547e4ec0c0630d03
mucahityuksel <m.yuksel793@gmail.com>
2025-08-30 23:19:22 +0300

    userform completed

--- src/app/(admin)/user/create/page.jsx
+++ src/app/(admin)/user/create/page.jsx
@@ -1,6 +1,13 @@
-import { Col, Container, FormControl, Row } from "react-bootstrap"
+'use client'
+import { useForm } from "react-hook-form";
+import TextAreaFormInput from "../../../../components/form/TextAreaFormInput"
+import { Col, Container, FormCheck, FormControl, FormSelect, Row } from "react-bootstrap"
+
 
 const UserCreate = () => {
+    const {
+        control
+    } = useForm();
     return <div className="d-flex flex-column jsutify-content-start">
         <Container>
             <Row>
@@ -21,6 +28,35 @@ const UserCreate = () => {
                         <div className="d-flex flex-column justify-content-start gap-1">
                             <label className="fs-6">Email Address</label>
                             <FormControl type="text" size="sm"></FormControl>
+                        </div>
+                        <div className="d-flex flex-column justify-content-start gap-1">
+                            <label className="fs-6">Phone Number</label>
+                            <FormControl type="text" size="sm"></FormControl>
+                        </div>
+                        
+                    </div>
+                </Col>
+                <Col xl={5}>
+                    <div className="d-flex flex-column justify-content-start gap-4">
+                        <div className="d-flex flex-column justify-content-start gap-1">
+                            <label className="fs-6">Birthday Date</label>
+                            <FormControl type="date" size="sm"></FormControl>
+                        </div>
+                        <div className="d-flex flex-column justify-content-start gap-1">
+                            <label className="fs-6">Gender</label>
+                            <FormSelect >
+                                <option value='male'>Male</option>
+                                <option value='female'>Female</option>
+                                <option value='none'>None</option>
+                            </FormSelect>
+                        </div>
+                        <div className="d-flex flex-column justify-content-start gap-1">
+                            <label className="fs-6">Bio</label>
+                            <TextAreaFormInput name="textarea"  control={control} rows={5} />
+                        </div>
+                        <div className="d-flex flex-column justify-content-start gap-1">
+                            <label className="fs-6">Status</label>
+                            <FormCheck type="switch"></FormCheck>
                         </div>
                     </div>
                 </Col>
